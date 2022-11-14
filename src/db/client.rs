6793cd67090722af583ced02ad78e8c3823db3f6
Chris <chrisliupascal@gmail.com>
2022-11-14 14:42:48 +0000

    Setup database client connection with sqlx and integrate with actix app state

--- src/db/client.rs
+++ src/db/client.rs
@@ -1,1 +1,13 @@
-use sqlx;
+use log::info;
+use sqlx::{postgres::PgPool, Pool, Postgres};
+
+pub async fn init_db_client(db_conn_url: String) -> Pool<Postgres> {
+    let db_client = PgPool::connect(db_conn_url.as_str()).await;
+    match db_client {
+        Ok(client) => {
+            info!("Successfully initiated database connection.");
+            client
+        }
+        Err(e) => panic!("Cannot initiate database connection. {}", e),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,40 @@
+mod db;
 mod init;
 
 use actix_web::{App, HttpServer};
 use dotenvy::var;
 use log::info;
+use sqlx::{Pool, Postgres};
+
+struct AppState {
+    db_client: Pool<Postgres>,
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     init::load_env_vars();
     init::init_logger();
 
+    // Initialize database client connection
+    let db_conn_url = var("DATABASE");
+    let db_client = match db_conn_url {
+        Ok(url) => db::client::init_db_client(url).await,
+        Err(_) => panic!("Database connection url is invalid."),
+    };
+
+    // Initialize web server
     let web_server_host = var("WEB_SERVER_HOST").unwrap_or(String::from("0.0.0.0"));
     let web_server_port = var("WEB_SERVER_PORT")
         .unwrap_or(String::from("3000"))
         .parse()
         .unwrap();
-    let server = HttpServer::new(move || App::new())
-        .bind((web_server_host.clone(), web_server_port))
-        .unwrap();
+    let server = HttpServer::new(move || {
+        App::new().app_data(AppState {
+            db_client: db_client.clone(),
+        })
+    })
+    .bind((web_server_host.clone(), web_server_port))
+    .unwrap();
 
     info!(
         "Web server listening on '{}:{}'",
